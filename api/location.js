49fd634c525137d9977fbb08d3d9844440c8d334
nam08102003 <ducnamle81003@gmail.com>
2022-12-17 11:16:18 +0700

    update

--- api/location.js
+++ api/location.js
@@ -34,7 +34,14 @@ router.get("/getlistDistrict/", async (req, res) => {
 
 router.post("/addoneDistrict", async (req, res) => {
   try {
-    res.status(200).json(req.body);
+    const data = req.body;
+    await DistrictsModel.create(data)
+      .then((result) => {
+        res.status(200).json("Thành công");
+      })
+      .catch((err) => {
+        res.status(500).json("Lỗi");
+      });
   } catch (err) {
     res.status(400).json(err);
   }
