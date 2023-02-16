8a0dc1b4afdc9dbfdba841182cbf9607677e408b
nam08102003 <ducnamle81003@gmail.com>
2023-02-16 11:54:12 +0700

    update

--- routes/pitch.js
+++ routes/pitch.js
@@ -68,21 +68,21 @@ router.get("/getall", async (req, res) => {
   try {
     await PitchsModel.find()
       .then((result) => {
-        const minMaxPrice = [];
-        for (let i = 0; i < result.length; i++) {
-          const arrayPrice = [];
-          for (let j = 0; j < result[i]?.listPitchs.length; j++) {
-            let listPitch = result[i]?.listPitchs[j];
-            for (let x = 0; x < listPitch?.infoPitchs.length; x++) {
-              const price = Number(listPitch?.infoPitchs[x]?.price);
-              arrayPrice.push(price);
-            }
-          }
-          minMaxPrice.push({
-            minPrice: Number(Math.min(...arrayPrice)),
-            maxPrice: Number(Math.max(...arrayPrice)),
-          });
-        }
+        // const minMaxPrice = [];
+        // for (let i = 0; i < result.length; i++) {
+        //   const arrayPrice = [];
+        //   for (let j = 0; j < result[i]?.listPitchs.length; j++) {
+        //     let listPitch = result[i]?.listPitchs[j];
+        //     for (let x = 0; x < listPitch?.infoPitchs.length; x++) {
+        //       const price = Number(listPitch?.infoPitchs[x]?.price);
+        //       arrayPrice.push(price);
+        //     }
+        //   }
+        //   minMaxPrice.push({
+        //     minPrice: Number(Math.min(...arrayPrice)),
+        //     maxPrice: Number(Math.max(...arrayPrice)),
+        //   });
+        // }
         res.status(200).json({
           success: true,
           message: "Thành công",
